207f5fd766e0c00fece57819bbb0cda8042c4405
Simon Schindler <simon@alle-schindlers.de>
2026-03-18 12:51:41 +0100

    fix EXG channels

--- src/pipeline/step01_loading.py
+++ src/pipeline/step01_loading.py
@@ -8,16 +8,12 @@ def load_data(bids_path: BIDSPath) -> RawEDF:
     raw: RawEDF = read_raw_bids(bids_path)
     raw.load_data()
 
-    # TODO: confirm?
-    # Apperantly only EXOG4 and EXG5 are EOG channels
-    # rename_map = {"EXG4": "EOG4", "EXG5": "EOG5"}
-    # raw.rename_channels(rename_map)
-
-    for ch in ["EXG4", "EXG5"]:
+    # rename EXG5 and EXG6 to EOG5 and EOG6
+    for ch in ["EXG5", "EXG6"]:
         raw.set_channel_types({ch: "eog"})
         raw.rename_channels({ch: ch.replace('X', 'O')})
 
-    for ch in ["EXG1", "EXG2", "EXG3", "EXG6", "EXG7", "EXG8"]:
+    for ch in ["EXG1", "EXG2", "EXG3", "EXG4", "EXG7", "EXG8"]:
         raw.set_channel_types({ch: "misc"})
 
     return raw
